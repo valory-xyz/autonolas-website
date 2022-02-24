11575ed4f3cf88751464be14d79dfa66f57b38ff
mohandast52 <mohandast52@gmail.com>
2022-02-24 16:18:50 +0530

    removed copy

--- components/HomePage/6AcceleratedLearning/index.jsx
+++ components/HomePage/6AcceleratedLearning/index.jsx
@@ -25,7 +25,6 @@ export const PROGRAM_DETAILS = [
   'Cohorts run approximately once a month',
   'Combination of guided lessons, 1:1 mentorship, exercises and project work',
   'Small groups (<10 people)',
-  'Financially subsidized participation',
 ];
 
 export const REQUIREMENTS = [

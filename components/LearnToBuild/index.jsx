a64a5fd2eb1d76114c59c9049fd625253c29d86a
mohandast52 <mohandast52@gmail.com>
2022-06-10 02:50:32 +0530

    videos page design added

--- components/LearnToBuild/index.jsx
+++ components/LearnToBuild/index.jsx
@@ -1,10 +1,18 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import Description from 'common-util/Description';
 import Button from 'common-util/Button';
+import { useCheckMobileScreen } from 'common-util/hooks';
 import {
-  Container, H3, VideoTable, ModulesFinished,
+  Container,
+  H3,
+  VideoTable,
+  ModulesFinished,
+  Hr,
+  DoThisTask,
 } from './styles';
 
-// TODO: length of video needs to be fixefd
+// TODO: length of video needs to be fixed
 
 const LIST = [
   {
@@ -20,7 +28,7 @@ const LIST = [
   {
     id: 'module-1',
     name: 'Module 1',
-    doThisTask: null,
+    doThisTask: ' ', // TODO: add do this task
     subList: [
       { no: 1, title: 'Intro to Agent-Based Development', length: '5:32' },
       { no: 2, title: 'AEA Components – Part 1', length: '10:12' },
@@ -31,7 +39,7 @@ const LIST = [
   {
     id: 'module-2',
     name: 'Module 2',
-    doThisTask: null,
+    doThisTask: ' ', // TODO: add do this task
     subList: [
       { no: 1, title: 'Intro to Multi-Agent Systems', length: '5:32' },
       { no: 2, title: 'Finite State Machines (FSMs)', length: '10:12' },
@@ -49,55 +57,102 @@ const LIST = [
 ];
 
 const LearnToBuild = () => {
-  console.log('first');
+  const isMobile = useCheckMobileScreen();
+
+  const handleApply = () => {
+    console.log('apply');
+  };
+
   return (
     <Container>
       <Description type={4} title="AGENT DEV ACADEMY – EDUCATION TRACK" />
-      <H3>Video Course</H3>
+      <Hr />
+      <H3>
+        <Image
+          src="/images/accelerated-learning/play-video.svg"
+          alt="Accelerated learning logo"
+          width={48}
+          height={isMobile ? 48 : 92}
+        />
+        <div>Video Course</div>
+      </H3>
 
       <div className="row row-1">
-        <div className="column-2">
-          <ModulesFinished>
-            <div className="card-header">Finished all the modules?</div>
-            <div className="card-body">
-              <p>
-                Develop autonomous services with other builders, guided by our
-                experts
-              </p>
+        {LIST.map(({
+          id, name, subList, doThisTask,
+        }, index) => (
+          <div key={id} className="each-video-section">
+            <div className="module-name">{name}</div>
+            <VideoTable>
+              <thead>
+                <tr>
+                  <th> </th>
+                  <th>Title</th>
+                  <th>Length</th>
+                </tr>
+              </thead>
+              <tbody>
+                {subList.map(({ no, title, length }) => (
+                  <tr>
+                    <td>{no}</td>
+                    <td>{title}</td>
+                    <td>{length}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </VideoTable>
 
+            {doThisTask ? (
+              <DoThisTask>
+                Do&nbsp;
+                <Link href="/learn-to-build-autonomous-services">
+                  <a
+                    rel="noopener noreferrer"
+                    href="/learn-to-build-autonomous-services"
+                  >
+                    this task
+                  </a>
+                </Link>
+                &nbsp;when you&apos;ve finished Module&nbsp;
+                {index}
+                &nbsp;videos
+              </DoThisTask>
+            ) : null}
+
+            <Hr />
+          </div>
+        ))}
+
+        <div className="row-2">
+          <ModulesFinished>
+            <div className="card-left">
+              <div className="logo">
+                <Image
+                  src="/images/accelerated-learning/tool-icon.svg"
+                  alt="Accelerated learning logo"
+                  width={120}
+                  height={120}
+                />
+              </div>
+
+              <div className="content">
+                <div className="title">Finished all the modules?</div>
+                <div className="desc">
+                  Develop autonomous services with other builders, guided by our
+                  experts
+                </div>
+              </div>
+            </div>
+
+            <div className="card-right">
               <Button
-                type="disabled"
+                // type="disabled"
                 className="btn-center"
                 title="Apply for Builder Track"
+                onClick={handleApply}
               />
             </div>
           </ModulesFinished>
-        </div>
-
-        <div className="column-1">
-          {LIST.map(({ id, name, subList }) => (
-            <div key={id} className="each-video-section">
-              <div className="module-name">{name}</div>
-              <VideoTable>
-                <thead>
-                  <tr>
-                    <th>No</th>
-                    <th>Title</th>
-                    <th>Length</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {subList.map(({ no, title, length }) => (
-                    <tr>
-                      <td>{no}</td>
-                      <td>{title}</td>
-                      <td>{length}</td>
-                    </tr>
-                  ))}
-                </tbody>
-              </VideoTable>
-            </div>
-          ))}
         </div>
       </div>
     </Container>
--- components/LearnToBuild/styles.jsx
+++ components/LearnToBuild/styles.jsx
@@ -1,70 +1,149 @@
 import styled from 'styled-components';
-// import { MEDIA_QUERY } from "util/theme";
+import { COLOR, MEDIA_QUERY } from 'util/theme';
+
+export const Hr = styled.hr`
+  margin: 0 -2rem;
+  height: 1px;
+  color: ${COLOR.BLACK};
+  background-color: ${COLOR.BLACK};
+  border: none;
+`;
 
 export const Container = styled.div`
-  padding: 2rem;
-  .row {
-    .column-1 {
-      .each-video-section {
-        margin-bottom: 3rem;
-        .module-name {
-          margin-bottom: 1rem;
-          font-size: 1.1rem;
-          font-weight: bolder;
-        }
+  padding: 0 2rem;
+  background: linear-gradient(
+    180deg,
+    ${COLOR.GREEN_2} 0%,
+    ${COLOR.WHITE} 20%,
+    ${COLOR.WHITE} 100%
+  );
+  .description {
+    padding: 2rem 0;
+    letter-spacing: 2px;
+  }
+  .row-1 {
+    font-size: 18px;
+    .each-video-section {
+      Hr {
+        margin-bottom: 2rem;
       }
+      .module-name {
+        font-family: triakis__fontheavy, sans-serif;
+        margin-bottom: 1rem;
+        font-size: 1.25rem;
+        font-weight: bolder;
+      }
+    }
+  }
+
+  ${MEDIA_QUERY.tablet} {
+    .description {
+      padding: 1rem 0;
+    }
+    .row-1 {
+      font-size: 16px;
     }
   }
 `;
 
 export const H3 = styled.h3`
-  margin: 0;
-  font-size: 78px;
-  line-height: normal;
-  font-family: triakis__fontheavy, sans-serif;
+  display: flex;
+  align-items: center;
+  margin: 1rem 0 2rem 0;
+  div {
+    display: inline-block;
+    margin-left: 2rem;
+    font-size: 78px;
+    font-family: triakis__fontheavy, sans-serif;
+  }
+
+  ${MEDIA_QUERY.tablet} {
+    div {
+      font-size: 34px;
+    }
+  }
 `;
 
 export const VideoTable = styled.table`
   border-collapse: collapse;
+  margin-bottom: 1.5rem;
   th {
     border-bottom: 1px solid black;
     text-align: left;
     font-weight: normal;
-    &:nth-child(1),
-    &:nth-child(3) {
-      width: 100px;
+    &:nth-child(1) {
+      width: 120px;
     }
     &:nth-child(2) {
-      width: 480px;
+      width: 520px;
     }
   }
   th,
   td {
-    padding: 0.3rem 0.5rem;
+    padding: 0.4rem 0.5rem;
+    &:nth-child(3) {
+      width: 60px;
+      text-align: right;
+    }
   }
   td {
+    &:nth-child(1) {
+      font-weight: bolder;
+    }
     &:nth-child(2) {
       text-decoration: underline;
-      text-underline-offset: 2px;
+      text-underline-offset: 3px;
     }
   }
 `;
 
-export const ModulesFinished = styled.div`
-  border: 1px solid black;
-  max-width: 280px;
-  padding: 1rem;
-  .card-header {
-    width: 10rem;
-    margin: 0 auto;
-    text-align: center;
-    line-height: normal;
-    font-size: 25px;
-    font-family: triakis__fontbold, sans-serif;
+export const DoThisTask = styled.div`
+  margin-bottom: 2rem;
+  a {
+    text-underline-offset: 3px;
+    color: ${COLOR.BLACK};
   }
-  .card-body {
-    width: 13rem;
-    margin: 1rem auto;
-    text-align: center;
+`;
+
+export const ModulesFinished = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 1rem 2.5rem 3rem 1rem;
+  .card-left {
+    display: flex;
+    .content {
+      max-width: 45%;
+      margin-left: 4rem;
+      .title {
+        line-height: 54px;
+        font-size: 70px;
+        font-family: triakis__fontsemibold, sans-serif;
+        margin-bottom: 1rem;
+      }
+      .desc {
+        font-size: 20px;
+      }
+    }
+  }
+  .card-right {
+    margin-top: 2rem;
+  }
+
+  ${MEDIA_QUERY.tablet} {
+    flex-direction: column;
+    padding: 0 0 2rem 0;
+    .card-left {
+      .content {
+        max-width: initial;
+        margin-left: 1rem;
+        .title {
+          line-height: 32px;
+          font-size: 38px;
+        }
+        .desc {
+          font-size: 16px;
+        }
+      }
+    }
   }
 `;

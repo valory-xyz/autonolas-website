b506909eccebb33af113ff3ccc46ac5e99a6dc9b
mohandast52 <mohandast52@gmail.com>
2022-06-14 16:02:17 +0530

    name fix

--- components/LearnToBuild/constants.jsx
+++ components/LearnToBuild/constants.jsx
@@ -35,7 +35,7 @@ const LIST = [
   {
     id: 'module-1',
     name: 'Module 1: Autonomous Economic Agents',
-    doThisTask: 'http://docs.google.com/document/d/1SnQ6XYDHlJgcHmi2biowVdIXjpZeDsJuYcFvmgm1u6I/export?format=pdf', // TODO: add do this task
+    doThisTask: 'https://docs.google.com/document/d/1SnQ6XYDHlJgcHmi2biowVdIXjpZeDsJuYcFvmgm1u6I/export?format=pdf', // TODO: add do this task
     subList: [
       {
         no: 1,
@@ -74,7 +74,7 @@ const LIST = [
   {
     id: 'module-2',
     name: 'Module 2: Agent Services',
-    doThisTask: 'http://docs.google.com/document/d/1SnQ6XYDHlJgcHmi2biowVdIXjpZeDsJuYcFvmgm1u6I/export?format=pdf', // TODO: add do this task
+    doThisTask: 'https://docs.google.com/document/d/1SnQ6XYDHlJgcHmi2biowVdIXjpZeDsJuYcFvmgm1u6I/export?format=pdf', // TODO: add do this task
     subList: [
       {
         no: 1,

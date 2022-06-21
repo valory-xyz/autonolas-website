573d1b0546a2e0d794ef22596e7c471f8e067f55
mohandast52 <mohandast52@gmail.com>
2022-06-22 00:40:46 +0530

    section-4

--- components/HomePage/4HowDoAutonolasWork/index.jsx
+++ components/HomePage/4HowDoAutonolasWork/index.jsx
@@ -1,54 +1,45 @@
 import React from 'react';
-import Button from 'common-util/Button';
 import Header from 'common-util/Header';
-import Description from 'common-util/Description';
-import { SectionFour } from './styles';
+import { SectionFour, Row } from './styles';
 
-const LIST = [
-  {
-    title: 'off-chain stack',
-    desc: 'For building autonomous apps with an open-source, agent-based, truly composable software framework.',
-  },
-  {
-    title: 'Smart Contract Chains and Apps',
-    desc: '',
-  },
-  {
-    title: 'on-chain protocol',
-    desc: 'For coordinating and securing the individual apps.',
-  },
+const DATA = [
+  'Services are made up of open-source software agents',
+  'Each agent is operated  independently',
+  'Services run continuously',
+  'Services get data from any data source',
+  'They can do complex stuff, even machine learning',
+  'Service agents robustly come to consensus about what action to take',
+  'Services take action on any chain or API',
 ];
 
 const WhatAreWeBuilding = () => (
-  <SectionFour className="section section-4 border-boxes">
-    <Header className="header" title="What are we building?" />
-    <div className="bg-image" />
-
-    <div className="row">
-      {LIST.map(({ title, desc }, index) => {
-        const getId = () => `what-we-are-building-${index}`;
-
-        return (
-          <div className={`column column-${index + 1}`} key={getId()}>
-            <div className="content">
-              <Description className="title" type={3} title={title} />
-              <Description className="text" type={4} title={desc} />
-            </div>
-          </div>
-        );
-      })}
-    </div>
-
-    <Button
-      type="disabled"
-      className="btn-center"
+  <SectionFour className="section section-4">
+    <Header
+      className="header"
       title={(
         <>
-          <span>Get Educated</span>
-          <span>(Coming Soon)</span>
+          How&nbsp;
+          <span className="sub-text">do</span>
+          &nbsp;autonomous service work?
         </>
       )}
     />
+
+    <Row>
+      <div className="column column-1" />
+
+      <div className="column column-2">
+        {DATA.map((item, index) => {
+          const getId = () => `how-autonolas-${index}`;
+          return (
+            <div className="content" key={getId()}>
+              <div className="sr-no">{index + 1}</div>
+              <div className="text">{item}</div>
+            </div>
+          );
+        })}
+      </div>
+    </Row>
   </SectionFour>
 );
 
--- components/HomePage/4HowDoAutonolasWork/styles.jsx
+++ components/HomePage/4HowDoAutonolasWork/styles.jsx
@@ -1,105 +1,48 @@
+import { backgroundImage } from 'components/GlobalStyles';
 import styled from 'styled-components';
-import { MEDIA_QUERY, COLOR } from 'util/theme';
+import { COLOR, MEDIA_QUERY } from 'util/theme';
+
+const IMG_URL = '/images/4HowDoAutonolasWork/how-autonomous-services-work.png';
 
 export const SectionFour = styled.div`
   position: relative;
-  background-image: url("/images/what-are-we-building/background.jpg");
   .header {
-    max-width: 586px;
-    margin-bottom: 166px !important;
-  }
-  .bg-image {
-    position: absolute;
-    top: 280px;
-    left: 50%;
-    height: 275px;
-    width: 500px;
-    transform: translate(-50%, 0%);
-    background-image: url("/images/what-are-we-building/image-1.png");
-    background-size: contain;
-    background-position: center;
-    background-repeat: no-repeat;
-  }
-  .row {
-    .column {
-      padding: 180px 0 48px 0 !important;
-      .content {
-        max-width: 64% !important;
-        /* align-items: flex-start !important; */
-        .title {
-          text-transform: uppercase;
-          max-width: 330px;
-        }
-        .text {
-          margin-top: 12px !important;
-          line-height: normal;
-        }
-      }
-      &.column-1,
-      &.column-3 {
-        .content {
-          align-items: flex-start !important;
-        }
-      }
-      &.column-2 {
-        .content {
-          max-width: 330px;
-          .title {
-            text-align: center;
-          }
-        }
-      }
-    }
-  }
-  .btn {
-    display: flex;
-    align-items: center;
-    flex-direction: column;
-    justify-content: center;
-    margin-top: 40px;
-  }
-  &.border-boxes .row .column {
-    border-color: ${COLOR.WHITE};
   }
 
   ${MEDIA_QUERY.tablet} {
-    background-image: url("/images/what-are-we-building/background-mobile.jpg");
-    .header {
-      margin-bottom: 54px !important;
-    }
-    .bg-image {
-      top: 120px;
-      width: 92%;
-    }
-    .row {
-      .column {
-        padding: 24px 16px !important;
-        &.column-1 {
-          min-height: 340px;
-          display: flex;
-          .content {
-            justify-content: flex-end;
-          }
-        }
-        &.column-2 {
-          .content {
-            align-items: flex-start;
-            .title {
-              text-align: left;
-            }
-          }
-        }
-      }
-    }
-    &.border-boxes .row .column {
-      &:nth-child(2) {
-        border-color: transparent ${COLOR.WHITE};
-      }
-    }
-    .btn {
-      margin-bottom: 32px;
-    }
   }
 `;
 
-export const EG = styled.div``;
+export const Row = styled.div`
+  display: flex;
+  align-items: stretch;
+  .column {
+  }
+  .column-1 {
+    flex: 1;
+    width: 100%;
+    ${backgroundImage(IMG_URL)}
+  }
+  .column-2 {
+    width: 27.5%;
+    padding-top: 8rem;
+
+    .content {
+      display: flex;
+      padding: 0.75rem 0 1rem 0;
+      border-left: 1px solid ${COLOR.BLACK};
+      &:not(:last-child) {
+        border-bottom: 1px solid ${COLOR.BLACK};
+      }
+      .sr-no {
+        font-family: neuemachina__light;
+        padding: 0.5rem 2rem 0 1.5rem;
+        font-size: 22px;
+        font-weight: bold;
+      }
+      .text {
+        padding-right: 2rem;
+      }
+    }
+  }
+`;

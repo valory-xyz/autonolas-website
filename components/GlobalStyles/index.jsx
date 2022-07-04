650ab4ee8911bac766549cd098e679cb60f15491
Oaksprout <oaksproutthetan@gmail.com>
2022-07-04 13:31:11 +0100

    removes column item truncation

--- components/GlobalStyles/index.jsx
+++ components/GlobalStyles/index.jsx
@@ -115,9 +115,6 @@ export const TwoColumnContents = styled.div`
       margin: 0.75rem 0 0.5rem 0;
       font-size: 28px;
       font-family: "manrope__semibold", sans-serif;
-      overflow: hidden;
-      text-overflow: ellipsis;
-      white-space: nowrap;
     }
     .sub-text {
       font-size: 18px;
@@ -187,9 +184,6 @@ export const ThreeColumnContents = styled.div`
       margin: 0.75rem 0 0.5rem 0;
       font-size: 28px;
       font-family: "manrope__semibold", sans-serif;
-      overflow: hidden;
-      text-overflow: ellipsis;
-      white-space: nowrap;
     }
     .sub-text {
       font-size: 18px;

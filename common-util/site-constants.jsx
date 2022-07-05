4d7ed948aed9a035994f0c48f302cbf4a3f3fd8d
Oaksprout <oaksproutthetan@gmail.com>
2022-07-05 15:37:05 +0100

    fix metatag image

--- common-util/site-constants.jsx
+++ common-util/site-constants.jsx
@@ -2,4 +2,4 @@ export const SITE_TAGLINE = 'Build Autonomous Services';
 export const SITE_TITLE = `Autonolas | ${SITE_TAGLINE}`;
 export const SITE_DESCRIPTION = 'Create, run and own autonomous services that power next-gen apps for crypto users and DAOs.';
 export const SITE_URL = 'https://autonolas.network';
-export const SITE_METATAG_IMAGE = '/images/site-metadata/site-metatag.png';
+export const SITE_METATAG_IMAGE = `${SITE_URL}/images/site-metadata/site-metatag.png`;

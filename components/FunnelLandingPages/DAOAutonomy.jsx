cb08d660c8840d5490871a2d7b4c7c1a559e5b63
mohandast52 <mohandast52@gmail.com>
2022-09-01 19:08:42 +0530

    remove old codes

--- components/FunnelLandingPages/DAOAutonomy.jsx
+++
@@ -1,6 +0,0 @@
-import generator from 'common-util/FunnelLandingPage/generator';
-import { FUNNELS } from 'util/constants/funnels';
-
-const DAOAutonomy = () => generator(FUNNELS, 'dao-autonomy');
-
-export default DAOAutonomy;
--- components/FunnelLandingPages/SmartProducts.jsx
+++
@@ -1,6 +0,0 @@
-import generator from 'common-util/FunnelLandingPage/generator';
-import { FUNNELS } from 'util/constants/funnels';
-
-const SmartProducts = () => generator(FUNNELS, 'smart-products');
-
-export default SmartProducts;

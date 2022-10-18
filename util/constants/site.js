893dc57d660792ad2c45ac5c3c956f0dc9e94b73
Mohan Das <mohandast52@gmail.com>
2022-10-18 13:27:19 +0530

    feat: "press" bug fix and metadata images size reduced (#46)
    * press to redirect to youtube instead of press page
    
    * metadata image fix
    
    * fix

--- util/constants/site.js
+++ util/constants/site.js
@@ -5,13 +5,13 @@ export const SITE_DESCRIPTION_PART_1 = 'Dev tools for creating novel software se
 export const SITE_DESCRIPTION_PART_2 = 'smart. Take your DAO, protocol or app into the next generation today.';
 
 export const SITE_DESCRIPTION = `${SITE_DESCRIPTION_PART_1} and ${SITE_DESCRIPTION_PART_2}`;
-export const SITE_METATAG_IMAGE = `${SITE_URL}/images/site-metadata/site-metatag.png`;
+export const SITE_METATAG_IMAGE = `${SITE_URL}/images/site-metadata/site-metatag.jpg`;
 
 export const META_TAGS_INFO = {
   siteUrl: SITE_URL,
   title: `Autonolas | ${SITE_TAGLINE}`,
   description: SITE_DESCRIPTION,
-  image: `${SITE_URL}/images/site-metadata/site-metatag.png`,
+  image: SITE_METATAG_IMAGE,
 };
 
 export const CUSTOM_META_PAGES = [

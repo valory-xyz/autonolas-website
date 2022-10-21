4f75a5748bc81fd6e00d0b8caecf03a154e36da8
mohandast52 <mohandast52@gmail.com>
2022-10-21 17:03:12 +0530

    update data

--- components/Academy/LandingPage/Academies/index.jsx
+++ components/Academy/LandingPage/Academies/index.jsx
@@ -4,41 +4,76 @@ import { AcademiesContainer } from './styles';
 
 const columns = [
   {
-    title: 'Name',
-    dataIndex: 'name',
-    key: 'name',
+    title: 'Cohort',
+    dataIndex: 'cohort',
+    key: 'cohort',
     render: text => getAnchor(text, 'https://twitter.com/autonolas'),
+    width: 300,
   },
   {
-    title: 'Age',
-    dataIndex: 'age',
-    key: 'age',
+    title: 'Participants',
+    dataIndex: 'participants',
+    key: 'participants',
+    render: value => (value || []).map(e => <div>{e.name}</div>),
+    width: 300,
   },
   {
-    title: 'Address',
-    dataIndex: 'address',
-    key: 'address',
+    title: 'Product',
+    dataIndex: 'product',
+    key: 'product',
+    render: value => (value || []).map(e => getAnchor(e.name, e.link)),
+  },
+  {
+    title: 'Quotes',
+    dataIndex: 'quotes',
+    key: 'quotes',
+    render: value => value,
   },
 ];
 
 const data = [
   {
     key: '1',
-    name: 'John Brown',
-    age: 32,
-    address: 'New York No. 1 Lake Park',
+    cohort: 'Academy 1',
+    participants: [{ name: 'A' }, { name: 'B' }, { name: 'C' }],
+    product: [{ name: 'El Collectooorr', link: 'https://elcollectooorr.art/' }],
+    quotes: '',
   },
   {
     key: '2',
-    name: 'Jim Green',
-    age: 42,
-    address: 'London No. 1 Lake Park',
+    cohort: 'Academy 2',
+    participants: [{ name: 'A' }, { name: 'B' }, { name: 'C' }],
+    product: [
+      {
+        name: 'Keeper Service',
+        link: 'https://github.com/valory-xyz/agent-academy-2',
+      },
+    ],
+    quotes: '',
   },
   {
     key: '3',
-    name: 'Joe Black',
-    age: 32,
-    address: 'Sidney No. 1 Lake Park',
+    cohort: 'Academy 3',
+    participants: [{ name: 'A' }, { name: 'B' }, { name: 'C' }],
+    product: [
+      {
+        name: 'Keeper Service II',
+        link: 'https://github.com/valory-xyz/agent-academy-3',
+      },
+    ],
+    quotes: '',
+  },
+  {
+    key: '4',
+    cohort: 'Academy 4',
+    participants: [{ name: 'A' }, { name: 'B' }, { name: 'C' }],
+    product: [
+      {
+        name: 'Autonomous service to forward RPC requests to specific endpoints',
+        link: 'https://github.com/valory-xyz/agent-academy-4',
+      },
+    ],
+    quotes: '',
   },
 ];
 

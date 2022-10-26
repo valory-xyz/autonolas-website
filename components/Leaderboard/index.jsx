c6369ca80c1c48ac4a0de064b095dacc20f07376
mohandast52 <mohandast52@gmail.com>
2022-10-26 16:27:16 +0530

    fix responsiveness

--- components/Leaderboard/index.jsx
+++ components/Leaderboard/index.jsx
@@ -1,50 +1,75 @@
-import React from 'react';
+import { Grid } from 'antd';
 import Header from 'common-util/Header';
 import Description from 'common-util/Description';
 import Button from 'common-util/Button';
 import { DescActionButtons } from '../HomePage/1TheNewWay/styles';
 import { LeaderboardContainer, LeaderboardContent } from './styles';
 
-const Leaderboard = () => (
-  <>
-    <LeaderboardContainer className="content-list-section">
-      <Header className="header" title="Community Leaderboard" />
-    </LeaderboardContainer>
+const { useBreakpoint } = Grid;
 
-    <LeaderboardContent className="section">
-      <Header className="header" title="Current Rankings" />
+const getSize = sizes => {
+  // till `lg` width will be 920
+  if (sizes.lg) {
+    return { width: 920, height: 425 };
+  }
 
-      <div className="leaderboard-table">
-        <iframe
-          width={920}
-          height={425}
-          title="leaderboard"
-          src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSuZsLhPIkleGOd4LIQL6gmJuZhsF0-6JcsqsVkZ08W5AAmIxkxO41aSUi5Csssf2z9IhfXspYCAy1o/pubhtml?gid=659479338&amp;single=true&amp;widget=true&amp;headers=false"
-        />
-      </div>
+  if (sizes.sm) {
+    return { width: 640, height: 500 };
+  }
 
-      <DescActionButtons>
-        <Description
-          type={2}
-          title={(
-            <>
-              To learn more about the Community Leaderboard, including ways you
-              can earn points and see your name up in lights, join us on the
-              Discord!
-            </>
-          )}
-        />
+  if (sizes.xs) {
+    return { width: 320, height: 400 };
+  }
 
-        <div className="action-btns">
-          <Button
-            title="JOIN DISCORD"
-            type="purple"
-            onClick={() => window.open('https://discord.gg/4xhAHuy4Y3')}
+  return { width: 920, height: 425 };
+};
+
+const Leaderboard = () => {
+  const screens = useBreakpoint();
+  const { width, height } = getSize(screens);
+
+  return (
+    <>
+      <LeaderboardContainer className="content-list-section">
+        <Header className="header" title="Community Leaderboard" />
+      </LeaderboardContainer>
+
+      <LeaderboardContent className="section">
+        <Header className="header" title="Current Rankings" />
+
+        <div className="leaderboard-table">
+          <iframe
+            style={width < 650 ? { width: '100%' } : {}}
+            width={width}
+            height={height}
+            title="leaderboard"
+            src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSuZsLhPIkleGOd4LIQL6gmJuZhsF0-6JcsqsVkZ08W5AAmIxkxO41aSUi5Csssf2z9IhfXspYCAy1o/pubhtml?gid=659479338&amp;single=true&amp;widget=true&amp;headers=false"
           />
         </div>
-      </DescActionButtons>
-    </LeaderboardContent>
-  </>
-);
+
+        <DescActionButtons>
+          <Description
+            type={2}
+            title={(
+              <>
+                To learn more about the Community Leaderboard, including ways
+                you can earn points and see your name up in lights, join us on
+                the Discord!
+              </>
+            )}
+          />
+
+          <div className="action-btns">
+            <Button
+              title="JOIN DISCORD"
+              type="purple"
+              onClick={() => window.open('https://discord.gg/4xhAHuy4Y3')}
+            />
+          </div>
+        </DescActionButtons>
+      </LeaderboardContent>
+    </>
+  );
+};
 
 export default Leaderboard;

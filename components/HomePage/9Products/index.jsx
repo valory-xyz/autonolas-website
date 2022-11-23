da95588f9e36cf756be2fd2bdbaeb0d76fda90bf
mohandast52 <mohandast52@gmail.com>
2022-11-23 18:38:39 +0530

    mobile fix

--- components/HomePage/9Products/index.jsx
+++ components/HomePage/9Products/index.jsx
@@ -1,15 +1,17 @@
-import { Col, Row } from 'antd';
-import Text from 'antd/lib/typography/Text';
-import Title from 'antd/lib/typography/Title';
-import CustomButton from 'common-util/Button';
-import Header from 'common-util/Header';
-import React from 'react';
+import {
+  Col, Row, Typography, Grid,
+} from 'antd';
 import Image from 'next/image';
+import Header from 'common-util/Header';
 import { COLOR } from 'util/theme';
-import { EcosystemServicesSection, HeadersRow } from './styles';
+import { ProductsSection, HeadersRow, ProductCard } from './styles';
 
-const BASE_IMAGES_PATH = '/images/10EcosystemServices/';
-const CORE = [
+const { Text } = Typography;
+const { useBreakpoint } = Grid;
+
+const BASE_IMAGES_PATH = '/images/9Products/';
+
+const FOR_DEVELOPERS_CORE = [
   {
     id: 'open-autonomy',
     imageFilename: 'open-autonomy-framework.png',
@@ -26,25 +28,9 @@ const CORE = [
     link: 'https://protocol.autonolas.network/',
     color: COLOR.PURPLE,
   },
-  {
-    id: 'el-collectooorr',
-    imageFilename: 'el-col.png',
-    title: 'El Collectooorr',
-    description: 'Get passive exposure to new generative art collections',
-    link: 'https://elcollectooorr.art/',
-    color: COLOR.GREEN_2,
-  },
-  {
-    id: 'autonolas-contribute',
-    imageFilename: 'alter-orbis.png',
-    title: 'Autonolas Contribute',
-    description:
-      'Strenghten the Autonolas ecosystem to earn points and upgrade your Alter Orbis character',
-    link: 'https://www.autonolas.network/community-leaderboard',
-    color: COLOR.GREEN_2,
-  },
+];
 
-  // 2nd part
+const FOR_DEVELOPERS_TOOLKITS = [
   {
     id: 'smart-managed-pools',
     imageFilename: 'smart-managed-pools.png',
@@ -63,43 +49,108 @@ const CORE = [
   },
 ];
 
-const Products = () => (
-  <EcosystemServicesSection id="products" className="section">
-    <Header title="Products" />
-    <HeadersRow>
-      <Col lg={12}>
-        <h3>For Developers</h3>
-      </Col>
-      <Col lg={12}>
-        <h3>For Users</h3>
-      </Col>
-    </HeadersRow>
+const FOR_USERS = [
+  {
+    id: 'el-collectooorr',
+    imageFilename: 'el-col.png',
+    title: 'El Collectooorr',
+    description: 'Get passive exposure to new generative art collections',
+    link: 'https://elcollectooorr.art/',
+    color: COLOR.GREEN_2,
+  },
+  {
+    id: 'autonolas-contribute',
+    imageFilename: 'alter-orbis.png',
+    imageStyle: { width: 80, height: 80 },
+    title: 'Autonolas Contribute',
+    description:
+      'Strenghten the Autonolas ecosystem to earn points and upgrade your Alter Orbis character',
+    link: 'https://www.autonolas.network/community-leaderboard',
+    color: COLOR.GREEN_2,
+  },
+];
 
-    <Row gutter={16} className="ecosystem-services-list" style={{ display: 'none' }}>
-      {CORE.map(ecosystemService => {
-        const {
-          id, imageFilename, title, description, link, buttonTitle,
-        } = ecosystemService;
+const getProductList = (list, type) => list.map(eachProduct => {
+  const {
+    id, imageFilename, title, description, link,
+  } = eachProduct;
 
-        return (
-          <Col xs={24} lg={12} key={id} className="ecosystem-service">
-            <Image
-              src={BASE_IMAGES_PATH + imageFilename}
-              className="ecosystem-service-image"
-              width="668px"
-              height="356px"
-            />
-            <Title className="ecosystem-service-title">{title}</Title>
-            <Text className="ecosystem-service-description">{description}</Text>
-            <br />
-            <a href={link} target="_blank" rel="noopener noreferrer">
-              <CustomButton title={buttonTitle} />
-            </a>
+  return (
+    <Col xs={24} sm={12} lg={12} key={id} className="product">
+      <ProductCard
+        target="_blank"
+        rel="noopener noreferrer"
+        href={link}
+        className={type}
+      >
+        <Image
+          src={BASE_IMAGES_PATH + imageFilename}
+          className="product-image"
+          width="96px"
+          height="96px"
+        />
+        <h3 className="product-title">{title}</h3>
+        <Text className="product-description">{description}</Text>
+      </ProductCard>
+    </Col>
+  );
+});
+
+const Products = () => {
+  const screens = useBreakpoint();
+  // console.log(screens);
+  const forUserHeader = <h3 className="product-title main-title">For Users</h3>;
+  return (
+    <ProductsSection id="products" className="section">
+      <Header title="Products" />
+
+      <HeadersRow>
+        <Col lg={12} sm={12} xs={24}>
+          <h3 className="product-title main-title">For Developers</h3>
+        </Col>
+        {!screens.xs && (
+          <Col lg={12} sm={12} xs={24}>
+            {forUserHeader}
           </Col>
-        );
-      })}
-    </Row>
-  </EcosystemServicesSection>
-);
+        )}
+      </HeadersRow>
+
+      <Row>
+        <Col lg={12} sm={12}>
+          <Row>
+            <Col lg={24} xs={24}>
+              <h3 className="product-title subtitle">Core</h3>
+            </Col>
+            {getProductList(FOR_DEVELOPERS_CORE, '')}
+
+            <Col lg={24} xs={24}>
+              <h3 className="product-title subtitle">Toolkits</h3>
+            </Col>
+            {getProductList(FOR_DEVELOPERS_TOOLKITS, '')}
+          </Row>
+        </Col>
+
+        <Col lg={12} sm={12}>
+          <HeadersRow>
+            {screens.xs && (
+              <Col xs={24}>
+                {forUserHeader}
+              </Col>
+            )}
+          </HeadersRow>
+
+          <Row>
+            {!screens.sm && (
+              <Col lg={24} xs={24}>
+                <h3 className="product-title subtitle"> </h3>
+              </Col>
+            )}
+            {getProductList(FOR_USERS, 'users')}
+          </Row>
+        </Col>
+      </Row>
+    </ProductsSection>
+  );
+};
 
 export default Products;

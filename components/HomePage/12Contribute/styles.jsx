4db579a70507f591fa168a2d7e59e642a353938a
mohandast52 <mohandast52@gmail.com>
2022-12-13 17:23:13 +0530

    mobile fix for contribute section

--- components/HomePage/12Contribute/styles.jsx
+++ components/HomePage/12Contribute/styles.jsx
@@ -1,10 +1,24 @@
 import styled from 'styled-components';
 import { backgroundImage } from 'components/GlobalStyles';
+import { MEDIA_QUERY } from 'util/theme';
 import { SectionOne } from '../1TheNewWay/styles';
 
 export const SectionContributeContainer = styled(SectionOne)`
+  ${backgroundImage('/images/12Contribute/background.jpg')}
   &.section {
     padding: 5rem 4.5rem !important;
   }
-  ${backgroundImage('/images/12Contribute/background.jpg')}
+
+  ${MEDIA_QUERY.mobileL} {
+    ${backgroundImage('/images/12Contribute/mobile-background.jpg')}
+    &.section {
+      padding: 3rem 1rem 6rem 1rem !important;
+      .description {
+        bottom: 160px;
+      }
+      .action-btns {
+        align-items: center;
+      }
+    }
+  }
 `;

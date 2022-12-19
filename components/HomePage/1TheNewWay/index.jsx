1cfc8d45d523836d07e089e86708bd7a37eb9cf5
Oaksprout <oaksproutthetan@gmail.com>
2022-12-19 15:53:20 +0100

    adds text rotation animation

--- components/HomePage/1TheNewWay/index.jsx
+++ components/HomePage/1TheNewWay/index.jsx
@@ -2,10 +2,31 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
 import Button from 'common-util/Button';
-import Description from 'common-util/Description';
+import { Typography } from 'antd';
 import Header from 'common-util/Header';
-import { SITE_DESCRIPTION, SITE_TAGLINE } from 'util/constants/site';
+import { SITE_TAGLINE } from 'util/constants/site';
+import ReactTextRotator from 'react-text-rotator';
 import { SectionOne, DescActionButtons } from './styles';
+
+const content = [
+  {
+    text: 'DAO Ops',
+    className: 'text-rotator-text',
+  },
+  {
+    text: 'DevOps',
+    className: 'text-rotator-text',
+  },
+  {
+    text: 'Product Ops',
+    className: 'text-rotator-text',
+  },
+  {
+    text: 'Growth Ops',
+    className: 'text-rotator-text',
+  },
+];
+
 
 const SectionOneBirthPlace = ({ isNavigationOpen }) => (
   <SectionOne
@@ -14,10 +35,16 @@ const SectionOneBirthPlace = ({ isNavigationOpen }) => (
     isNavigationOpen={isNavigationOpen}
   >
     <Header className="header" title={SITE_TAGLINE} as="h1" />
-
     <DescActionButtons>
-      <Description type={2} title={SITE_DESCRIPTION} />
-
+      <Typography.Title level={4} className="lead">
+        Our open-source software stack enables you to run your
+        {' '}
+        <div className="text-rotator-container">
+          <ReactTextRotator content={content} time={2500} startDelay={250} transitionTime={500} />
+        </div>
+        {' '}
+        as a unified, decentralized off-chain system.
+      </Typography.Title>
       <div className="action-btns">
         <Link href="/#ecosystem-builders" passHref>
           <a>

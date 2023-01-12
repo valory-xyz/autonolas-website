346311dcd7c35a4d60547b37325099f8a487effc
mohandast52 <mohandast52@gmail.com>
2023-01-12 17:03:17 +0530

    redirect autonolas-asset-management-info TO products/smart-managed-pool

--- pages/autonomous-asset-management-infra.jsx
+++ pages/autonomous-asset-management-infra.jsx
@@ -1,30 +1,8 @@
-import FunnelLandingPage from 'common-util/FunnelLandingPage';
-import AssetManagement from 'common-util/FunnelLandingPage/6AssetManagement';
-import { getEducationArticles, getFunnel } from 'common-util/api';
-import { serverRedirectToError } from 'common-util/functions';
-
 export async function getServerSideProps() {
-  const educationArticles = await getEducationArticles();
-  const funnel = await getFunnel(6);
-
-  if (!funnel) {
-    return serverRedirectToError();
-  }
-
   return {
-    props: {
-      funnel,
-      educationArticles: educationArticles.slice(0, 3) /* only 3 items */,
-      name: 'autonomous-asset-management-infra',
+    redirect: {
+      destination: 'products/smart-managed-pools',
+      permanent: true,
     },
   };
 }
-
-const AssetManagementInfra = props => (
-  <>
-    <FunnelLandingPage {...props} />
-    <AssetManagement {...props} />
-  </>
-);
-
-export default AssetManagementInfra;

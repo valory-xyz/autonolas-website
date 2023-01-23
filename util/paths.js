5cdf253f6257bbd2001dea078258f941cb8f9afa
Oaksprout <oaksproutthetan@gmail.com>
2023-01-23 16:04:03 +0000

    dry out paths

--- util/paths.js
+++ util/paths.js
@@ -1,3 +1,5 @@
+import { SITE_URL } from './constants/site';
+
 export const PATHS = {
   EDUCATION_ARTICLES: 'education-articles',
   IDEAS: 'ideas',
@@ -7,5 +9,11 @@ export const PATHS = {
   PRIVACY_POLICY: 'privacy-policy',
   COOKIES_POLICY: 'cookies-policy',
 };
+
+const DOCS_URL = 'https://docs.autonolas.network';
+export const BUILDER_HELP_URL = 'https://propel.valory.xyz';
+
+export const PRODUCT_PATH = `${SITE_URL}/product/`;
+export const PRODUCT_DOCS_PATH = `${DOCS_URL}/product/`;
 
 export default PATHS;

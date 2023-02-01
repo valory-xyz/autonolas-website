75cb738cf3c6a6d5a4086c54f2c728bba21223ec
mohandast52 <mohandast52@gmail.com>
2023-02-01 17:43:32 +0530

    metatag fix

--- components/Whitepaper/index.jsx
+++ components/Whitepaper/index.jsx
@@ -1,57 +1,42 @@
 import { Col, Row, Typography } from 'antd';
 import { LinkCard } from 'components/GlobalStyles';
-import Meta from 'common-util/meta';
-import { getHostName } from 'common-util/functions';
-import { SITE_URL } from 'util/constants/site';
 import { Hero } from './styles';
 
 const { Title } = Typography;
 
-const pageSettings = {
-  pathname: '/whitepaper',
-  title: 'Whitepaper',
-  description:
-    "Do a deep-dive into Autonolas' vision for autonomy, technical architecture, tokenomics, use cases and governance.",
-  imagePath: `${getHostName()}/images/whitepaper/autonolas-whitepaper-meta.png`,
-};
-
 const Whitepaper = () => (
-  <>
-    <Meta
-      meta={{
-        siteUrl: `${SITE_URL}${pageSettings.pathname}`,
-        title: `${pageSettings.title} | Autonolas`,
-        description: pageSettings.description,
-        image: pageSettings.imagePath,
-      }}
+  <Hero>
+    <Title className="title">Whitepaper</Title>
+    <Row gutter={24}>
+      <Col lg={{ span: 6, offset: 6 }} md={{ span: 8, offset: 4 }} xs={24}>
+        <a href="/documents/whitepaper/autonolas-whitepaper-summary.pdf">
+          <LinkCard>
+            <img
+              src="/images/whitepaper/autonolas-whitepaper-summary.svg"
+              alt="Autonolas whitepaper summary"
+            />
+            <Title level={2}>Summary</Title>
+          </LinkCard>
+        </a>
+      </Col>
+      <Col lg={6} md={8} xs={24}>
+        <a href="/documents/whitepaper/autonolas-whitepaper.pdf">
+          <LinkCard>
+            <img
+              src="/images/whitepaper/autonolas-whitepaper.svg"
+              alt="Autonolas whitepaper"
+            />
+            <Title level={2}>Whitepaper</Title>
+          </LinkCard>
+        </a>
+      </Col>
+    </Row>
+    <img
+      className="image"
+      src="/images/pages/whitepaper/glass-element.png"
+      alt="Glass object, encapsulating complexity"
     />
-    <Hero>
-      <Title className="title">Whitepaper</Title>
-      <Row gutter={24}>
-        <Col lg={{ span: 6, offset: 6 }} md={{ span: 8, offset: 4 }} xs={24}>
-          <a href="/documents/whitepaper/autonolas-whitepaper-summary.pdf">
-            <LinkCard>
-              <img src="/images/whitepaper/autonolas-whitepaper-summary.svg" alt="Autonolas whitepaper summary" />
-              <Title level={2}>Summary</Title>
-            </LinkCard>
-          </a>
-        </Col>
-        <Col lg={6} md={8} xs={24}>
-          <a href="/documents/whitepaper/autonolas-whitepaper.pdf">
-            <LinkCard>
-              <img src="/images/whitepaper/autonolas-whitepaper.svg" alt="Autonolas whitepaper" />
-              <Title level={2}>Whitepaper</Title>
-            </LinkCard>
-          </a>
-        </Col>
-      </Row>
-      <img
-        className="image"
-        src="/images/pages/whitepaper/glass-element.png"
-        alt="Glass object, encapsulating complexity"
-      />
-    </Hero>
-  </>
+  </Hero>
 );
 
 export default Whitepaper;

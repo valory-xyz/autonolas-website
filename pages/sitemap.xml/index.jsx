b293bdf1fbd14af8dd495134cd14ff5f1b106ffb
mohandast52 <mohandast52@gmail.com>
2023-02-10 03:19:09 +0530

    static paths added

--- pages/sitemap.xml/index.jsx
+++ pages/sitemap.xml/index.jsx
@@ -1,37 +1,25 @@
-// import { SITE_URL } from 'util/constants/site';
-import { staticPaths } from './readingfile';
+import { getStaticPaths, getDynamicPaths } from './readingfile';
 
-const Sitemap = () => {
-  const allPaths = [...staticPaths];
-  // const allPaths = [...staticPaths, ...dynamicPaths];
+export const getServerSideProps = async ({ res }) => {
+  const staticPaths = await getStaticPaths();
+  const dynamicPaths = await getDynamicPaths();
+
+  const allPaths = [...staticPaths, ...dynamicPaths];
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-      ${allPaths
+    ${allPaths
     .map(
       url => `
-            <url>
-              <loc>${url}</loc>
-              <lastmod>${new Date().toISOString()}</lastmod>
-              <changefreq>monthly</changefreq>
-              <priority>1.0</priority>
-            </url>
-          `,
+        <url>
+          <loc>${url}</loc>
+          <lastmod>${new Date().toISOString()}</lastmod>
+          <changefreq>monthly</changefreq>
+          <priority>1.0</priority>
+        </url>
+      `,
     )
     .join('')}
-    </urlset>
-`;
-
-  return sitemap;
-};
-
-export const getServerSideProps = async ({ res }) => {
-  // const BASE_URL = 'http://localhost:3000';
-
-  const sitemap = `
-    <?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-      // our URL's will go here
     </urlset>
   `;
 
@@ -43,5 +31,7 @@ export const getServerSideProps = async ({ res }) => {
     props: {},
   };
 };
+
+const Sitemap = () => null;
 
 export default Sitemap;
---
+++ pages/sitemap.xml/readingfile.jsx
@@ -0,0 +1,53 @@
+import glob from 'glob';
+import { chain } from 'lodash';
+import { SITE_URL } from 'util/constants/site';
+
+export const getStaticPaths = async () => {
+  const pagesDir = 'pages/**/*.jsx';
+  const pagesPaths = await glob.sync(pagesDir);
+
+  const filteredPaths = pagesPaths.filter(
+    path => !path.includes('/_')
+      && !path.includes('/[')
+      && !path.includes('api')
+      && !path.includes('sitemap'),
+  );
+
+  /**
+   * replacedPaths is an array of all the paths in the pages directory
+   * eg: ['/pages/index.jsx', '/pages/academy/index.jsx'].
+   * Hence we need to replace the `pages`, `/index.jsx` or `.jsx`
+   */
+  const replacedPaths = chain(filteredPaths)
+    .map(path => path.replace('pages', '').replace('/index.jsx', '').replace('.jsx', ''))
+    .value();
+
+  /**
+   * paths is an array of all the paths in the pages directory
+   * eg: [
+   *  'https://autonolas.network',
+   *  'https://autonolas.network/academy',
+   *  'https://autonolas.network/dao-autonomy',
+   * ]
+   */
+  const paths = replacedPaths.map(
+    staticPagePath => `${SITE_URL}${staticPagePath}`,
+  );
+
+  // TODO: fix it
+  return [''] || paths;
+};
+
+export const getDynamicPaths = async () => {
+  const pagesDir = 'pages/**/*.jsx';
+  const pagesPaths = await glob.sync(pagesDir);
+
+  const filteredPaths = pagesPaths.filter(
+    path => path.includes('/[') && !path.includes('/_'),
+  );
+
+  console.log(filteredPaths);
+
+  // TODO: fix it
+  return [''] || filteredPaths;
+};

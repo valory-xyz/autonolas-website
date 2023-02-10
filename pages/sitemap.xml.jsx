c281ccb1ad92e5df32583be967ae4e6fd67e3a13
mohandast52 <mohandast52@gmail.com>
2023-02-10 05:37:08 +0530

    dir = './pages/**/*.jsx'

--- pages/sitemap.xml.jsx
+++ pages/sitemap.xml.jsx
@@ -2,7 +2,7 @@ import { getStaticPaths } from 'common-util/sitemapHelpers/staticPaths';
 import { getDynamicPaths } from 'common-util/sitemapHelpers/dynamicPaths';
 
 export const getServerSideProps = async ({ res }) => {
-  const BASE_DIR = process.env.NODE_ENV === 'production' ? './**/*.jsx' : 'pages/**/*.jsx';
+  const BASE_DIR = process.env.NODE_ENV === 'production' ? './pages/**/*.jsx' : 'pages/**/*.jsx';
 
   const staticPaths = await getStaticPaths(BASE_DIR);
   const dynamicPaths = await getDynamicPaths();

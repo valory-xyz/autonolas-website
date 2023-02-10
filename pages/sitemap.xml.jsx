844aa4f7cd679d3ceacdbb93246e2db20f77b505
mohandast52 <mohandast52@gmail.com>
2023-02-11 03:14:39 +0530

    fix

--- pages/sitemap.xml.jsx
+++ pages/sitemap.xml.jsx
@@ -1,10 +1,11 @@
-import {
-  // getStaticPaths,
+// import {
+//   // getStaticPaths,
 
-  staticPathsOther,
-} from 'common-util/sitemapHelpers/staticPaths';
+//   staticPathsOther,
+// } from 'common-util/sitemapHelpers/staticPaths';
+import globby from 'globby';
+
 import { getDynamicPaths } from 'common-util/sitemapHelpers/dynamicPaths';
-import path from 'path';
 
 
 export const triedPathForConfig = [
@@ -14,21 +15,30 @@ export const triedPathForConfig = [
   '**/*.jsx',
 ];
 
-// write a function getStaticSideProps to fetch all the file names inside "pages" folder
-// and return them as an array of strings
-export const getStaticProps = async ({ res }) => {
-  // fetch files from vercel production
+export const getServerSideProps = async ({ res }) => {
+  // const BASE_DIR = process.env.NODE_ENV.toLowerCase() === 'production'
+  //   ? '**/*.jsx'
+  //   : 'pages/**/*.jsx';
+
+  // const BASE_DIR = `${process.cwd()}/pages/**/*.jsx`;
+
+  // const staticPaths = await getStaticPaths(BASE_DIR);
   const dynamicPaths = await getDynamicPaths();
 
   // TRY
-  const staticPaths = await staticPathsOther(
-    process.env.NODE_ENV.toLowerCase() === 'production' ? path.join(process.cwd(), './next') : 'pages',
-  );
+  const staticPaths = await globby([
+    'pages/*.(t|j)sx',
+    '!pages/_*.(t|j)sx', // for _app.tsx and _document.tsx
+    '!pages/[*.(t|j)sx', // for [...page].tsx and [[...page]].tsx
+    '!pages/api',
+    '!pages/404.(t|j)sx',
+    '!pages/500.(t|j)sx',
+  ]);
 
   const allPaths = [...staticPaths, ...dynamicPaths];
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"> 
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
     ${allPaths
     .map(
       url => `
@@ -51,50 +61,6 @@ export const getStaticProps = async ({ res }) => {
     props: {},
   };
 };
-
-
-// export const getServerSideProps = async ({ res }) => {
-//   // const BASE_DIR = process.env.NODE_ENV.toLowerCase() === 'production'
-//   //   ? '**/*.jsx'
-//   //   : 'pages/**/*.jsx';
-
-//   // const BASE_DIR = `${process.cwd()}/pages/**/*.jsx`;
-
-//   // const staticPaths = await getStaticPaths(BASE_DIR);
-//   const dynamicPaths = await getDynamicPaths();
-
-//   // TRY
-//   const staticPaths = await staticPathsOther(
-//     process.env.NODE_ENV.toLowerCase() === 'production' ?
-// path.join(process.cwd(), './') : 'pages',
-//   );
-
-//   const allPaths = [...staticPaths, ...dynamicPaths];
-
-//   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-//     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
-//     ${allPaths
-//     .map(
-//       url => `
-//         <url>
-//           <loc>${url}</loc>
-//           <lastmod>${new Date().toISOString()}</lastmod>
-//           <priority>1.0</priority>
-//         </url>
-//       `,
-//     )
-//     .join('')}
-//     </urlset>
-//   `;
-
-//   res.setHeader('Content-Type', 'text/xml');
-//   res.write(sitemap);
-//   res.end();
-
-//   return {
-//     props: {},
-//   };
-// };
 
 const Sitemap = () => null;
 

2f47907946190309a443e3e384d36756aeeb9cba
mohandast52 <mohandast52@gmail.com>
2023-02-11 01:35:33 +0530

    fix

--- pages/sitemap.xml.jsx
+++ pages/sitemap.xml.jsx
@@ -24,7 +24,7 @@ export const getServerSideProps = async ({ res }) => {
 
   // TRY
   const staticPaths = await staticPathsOther(
-    process.env.NODE_ENV.toLowerCase() === 'production' ? './' : 'pages',
+    process.env.NODE_ENV.toLowerCase() === 'production' ? './pages' : 'pages',
   );
 
   const allPaths = [...staticPaths, ...dynamicPaths];

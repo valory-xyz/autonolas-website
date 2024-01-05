923a17839daa2a444aa82b115384d4690f97e6be
Oaksprout <66292936+oaksprout@users.noreply.github.com>
2024-01-05 18:25:37 +0000

    Refactor Markdown component styling

--- common-util/Markdown/index.jsx
+++ common-util/Markdown/index.jsx
@@ -2,15 +2,24 @@ import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import rehypeRaw from 'rehype-raw';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const StyledMarkdown = styled(ReactMarkdown)`
+  & pre {
+    font-size: 16px;
+    word-break: break-word;
+    white-space: pre-wrap;
+  }
+`;
 
 const Markdown = ({ children }) => (
-  <ReactMarkdown
+  <StyledMarkdown
     remarkPlugins={[remarkGfm]}
     rehypePlugins={[rehypeRaw]}
     transformImageUri={uri => (uri.startsWith('http') ? uri : `${process.env.NEXT_PUBLIC_API_URL}${uri}`)}
   >
     {children}
-  </ReactMarkdown>
+  </StyledMarkdown>
 );
 
 Markdown.propTypes = {

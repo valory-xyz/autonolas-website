9b26af85a2405b9d792042766cb8dc9fa92d5cee
Oaksprout <66292936+oaksprout@users.noreply.github.com>
2024-01-22 14:38:51 +0000

    Update file paths and URLs for whitepaper documents

--- next.config.js
+++ next.config.js
@@ -3,29 +3,30 @@ const withAntdLess = require('next-plugin-antd-less');
 const OLAS_URL = 'https://olas.network';
 const WHITEPAPER_DOC_PATH = '/documents/whitepaper';
 const OLAS_WHITEPAPER_DOC_URL = `${OLAS_URL}${WHITEPAPER_DOC_PATH}`;
-const WHITEPAPER_FILE_PATH = '/Whitepaper v1.0.pdf';
-const WHITEPAPER_SUMMARY_FILE_PATH = '/Whitepaper Summary v1.0.pdf';
+const WHITEPAPER_FILE_PATH = '/Whitepaper%20v1.0.pdf';
+const WHITEPAPER_SUMMARY_FILE_PATH = '/Whitepaper%20Summary%20v1.0.pdf';
 const TOKENOMICS_PAPER_FILE_PATH = '/Autonolas_Tokenomics_Core_Technical_Document.pdf';
+const WHITEPAPER_PATH = '/whitepaper';
 
 const redirects = [
   {
-    source: `${WHITEPAPER_DOC_PATH}${WHITEPAPER_FILE_PATH}`,
-    destination: `${OLAS_WHITEPAPER_DOC_URL}${WHITEPAPER_FILE_PATH}`,
+    source: `${WHITEPAPER_DOC_PATH}${WHITEPAPER_FILE_PATH}`, // /documents/whitepaper/Whitepaper v1.0.pdf
+    destination: `${OLAS_WHITEPAPER_DOC_URL}${WHITEPAPER_FILE_PATH}`, // https://olas.network/documents/whitepaper/Whitepaper v1.0.pdf
     permanent: true,
   },
   {
-    source: `${WHITEPAPER_DOC_PATH}${WHITEPAPER_SUMMARY_FILE_PATH}`,
-    destination: `${OLAS_WHITEPAPER_DOC_URL}${WHITEPAPER_SUMMARY_FILE_PATH}`,
+    source: `${WHITEPAPER_DOC_PATH}${WHITEPAPER_SUMMARY_FILE_PATH}`, // /documents/whitepaper/Whitepaper Summary v1.0.pdf
+    destination: `${OLAS_WHITEPAPER_DOC_URL}${WHITEPAPER_SUMMARY_FILE_PATH}`, // https://olas.network/documents/whitepaper/Whitepaper Summary v1.0.pdf
     permanent: true,
   },
   {
-    source: `${WHITEPAPER_DOC_PATH}${TOKENOMICS_PAPER_FILE_PATH}`,
-    destination: `${OLAS_WHITEPAPER_DOC_URL}${TOKENOMICS_PAPER_FILE_PATH}`,
+    source: `${WHITEPAPER_DOC_PATH}${TOKENOMICS_PAPER_FILE_PATH}`, // /documents/whitepaper/Autonolas_Tokenomics_Core_Technical_Document.pdf
+    destination: `${OLAS_WHITEPAPER_DOC_URL}${TOKENOMICS_PAPER_FILE_PATH}`, // https://olas.network/documents/whitepaper/Autonolas_Tokenomics_Core_Technical_Document.pdf
     permanent: true,
   },
   {
-    source: '/whitepaper',
-    destination: `${OLAS_URL}/whitepaper`,
+    source: `${WHITEPAPER_PATH}`,
+    destination: `${OLAS_URL}${WHITEPAPER_PATH}`,
     permanent: true,
   },
 ];
